5bfc44c6fca05bf63db1e1bd276afc6baa3f8900
Paulo Serafim <paserafim@gmail.com>
2021-04-02 19:27:35 +0100

    Added Vets List and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/paserafim/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/paserafim/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,23 @@
 package paserafim.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import paserafim.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index" ,"/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
