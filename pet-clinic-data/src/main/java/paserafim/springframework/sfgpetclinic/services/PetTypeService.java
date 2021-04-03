e9b1a8a7287ee8a8b461996938e3d8a4c1a32ced
Paulo Serafim <paserafim@gmail.com>
2021-04-03 17:15:37 +0100

    Added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/paserafim/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package paserafim.springframework.sfgpetclinic.services;
+
+import paserafim.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
