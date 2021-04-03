f87c6886f93e4996ce0ab1ae7b33f959e2b2d2d9
Paulo Serafim <paserafim@gmail.com>
2021-04-03 17:00:22 +0100

    Create Vet Specialty Entity, Associate to Vet. closes #27

--- pet-clinic-data/src/main/java/paserafim/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/paserafim/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package paserafim.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Specialty> specialty;
+
+    public Set<Specialty> getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Set<Specialty> specialty) {
+        this.specialty = specialty;
+    }
 }
